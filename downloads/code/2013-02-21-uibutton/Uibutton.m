6d92b5f8b34133246c57f6e8f9b2c5b11737a291
Masahiko Sato <masa2sei@gmail.com>
2013-02-21 15:53:44 +0900

    Site updated at 2013-02-21 06:53:44 UTC

---
+++ downloads/code/2013-02-21-uibutton/Uibutton.m
@@ -0,0 +1,27 @@
+//UIbuttonの生成
+UIButton* btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+//ボタンの表示位置とサイズの設定
+btn.frame = CGRectMake(10, 10, 100, 30);
+//ボタンのタイトル
+[btn setTitle:@"ボタンのタイトル" forState:UIControlStateNormal];
+//ボタンがタッチダウンされたときに呼び出されるメソッドを設定
+[btn addTarget:self action:@selector(hoge:)
+     forControlEvents:UIControlEventTouchDown];
+//サブビューに追加
+[self.view addSubview:btn];
+
+
+//画像を指定したボタン
+UIImage* img [UIImage imageNamed:@"hoge"];
+UIButton *btn = [[UIButton alloc] itinitWithFrame:CGRectMake(10, 10, 100, 30)];
+//背景に画像をセット
+[btn setBackgroundImage:img forState:UIControlStateNormal];
+[btn addTarget:self
+        action:@selector(hoge:) forControlEvents:UIControlEventTouchUpInside];
+
+
+
+// 呼ばれるhogeメソッド
+-(void)hoge:(id)sender{
+        // ここに何かの処理を記述する
+}
